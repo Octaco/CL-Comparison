d1d5068e12b32b9b63a52c5902afdcdf39113cbf
Leo <leonard.remus@uni-ulm.de>
2024-02-07 21:27:20 +0100

    fixed basic_run.sh

--- run_scripts/basic_run.sh
+++ run_scripts/basic_run.sh
@@ -5,14 +5,14 @@ GPU=$1
 
 python3 ./run.py \
     --loss_function INFO_NCE \
-    --log_path ../logging \
+    --log_path ./logging \
     --lang ruby \
     --train_batch_size 7 \
     --eval_batch_size 7 \
     --learning_rate 1e-5 \
     --num_train_epochs 5 \
     --train_size 0.8 \
-    --mrr_path ../data/MRR.txt \
+    --mrr_path ./data/MRR.txt \
     --log_level INFO \
     --num_of_accumulation_steps 10 \
     --GPU "$GPU"
