15df8fa681f5efc64c587632a15bebddcecc752e
Leo <leonard.remus@uni-ulm.de>
2024-02-23 23:11:50 +0100

    changed hyperparameter

--- run_scripts/basic_run.sh
+++ run_scripts/basic_run.sh
@@ -7,12 +7,12 @@ python3 ./run.py \
     --loss_function INFO_NCE \
     --log_path ./logging \
     --lang ruby \
-    --train_batch_size 7 \
-    --eval_batch_size 7 \
+    --train_batch_size 16 \
+    --eval_batch_size 16 \
     --learning_rate 1e-5 \
     --num_train_epochs 5 \
     --train_size 0.8 \
     --mrr_path ./data/MRR.txt \
     --log_level INFO \
-    --num_of_accumulation_steps 10 \
+    --num_of_accumulation_steps 16 \
     --GPU "$GPU"
