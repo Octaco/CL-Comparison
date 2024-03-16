8217aa51a90d964575b0882fff00712234748974
Octacon <leo.remus@me.com>
2024-03-16 13:19:21 +0100

    more run_scripts

---
+++ run_scripts/uni_CL.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Run the Python script with specified arguments
+
+GPU=$1
+
+python3 ./run.py \
+    --loss_function ContrastiveLoss \
+    --architecture SimCLR \
+    --data_path ./data \
+    --lang ruby \
+    --train_batch_size 16 \
+    --eval_batch_size 16 \
+    --learning_rate 1e-5 \
+    --num_train_epochs 5 \
+    --train_size 0.8 \
+    --data_path ./data/ \
+    --log_level INFO \
+    --num_of_accumulation_steps 16 \
+    --num_of_distractors 99 \
+    --GPU "$GPU"
