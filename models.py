101f68a11338295542e2a6350e8563cbe3885037
lremus <leonard.remus@uni-ulm.de>
2024-03-26 22:17:49 +0100

    own moco code

--- models.py
+++ models.py
@@ -37,6 +37,12 @@ class BiEncoderModel(torch.nn.Module):
 
 
 class MoCoModel(torch.nn.Module):
+    """
+    dim = max model dim
+    k = queue length
+    m = momentum
+    """
+
     def __init__(self, model_name, dim=768, k=4096, m=0.999):
         super(MoCoModel, self).__init__()
         self.k = k
@@ -44,51 +50,47 @@ class MoCoModel(torch.nn.Module):
         self.model_q = RobertaModel.from_pretrained(model_name)
         self.model_k = RobertaModel.from_pretrained(model_name)
         self._momentum_update()
-        self.projection_head = torch.nn.Sequential(
-            torch.nn.Linear(self.model_q.config.hidden_size, dim))
 
         # queue
-        self.register_buffer("queue", torch.randn(dim, k))
-        self.queue = torch.nn.functional.normalize(self.queue, dim=0)
-
-        # queue pointer
-        self.pointer = 0
+        self.queue = []
 
     def _dequeue_and_enqueue(self, keys):
-        # gather keys before updating
-        keys = keys.detach()
-        batch_size = keys.shape[0]
 
-        # replace the keys at the pointer
-        self.queue[:, self.pointer:self.pointer + batch_size] = keys.T
-        self.pointer = (self.pointer + batch_size) % self.k
+        if len(self.queue) < self.k:
+            self.queue.append(keys)
+        else:
+            self.queue.pop(0)
+            self.queue.append(keys)
+
+        return self.queue
 
     def _momentum_update(self):
         for param_q, param_k in zip(self.model_q.parameters(), self.model_k.parameters()):
-            param_k.data = param_k.data * self.m + param_q.data * (1. - self.m)
+            param_k.data = param_k.data * self.m + param_q.data * (1 - self.m)
             param_k.requires_grad = False
 
-    def forward(self, input_ids, attention_mask):
-        q = self.model_q(input_ids=input_ids, attention_mask=attention_mask)
-        q = self.projection_head(q.pooler_output)
-        q = torch.nn.functional.normalize(q, dim=1)
+    def forward(self, is_code_key, input_ids, attention_mask):
 
-        with torch.no_grad():
-            self._momentum_update()
-            k = self.model_k(input_ids=input_ids, attention_mask=attention_mask)
-            k = self.projection_head(k.pooler_output)
-            k = torch.nn.functional.normalize(k, dim=1)
+        if is_code_key:
+            with torch.no_grad():
+                self._momentum_update()
+                k = self.model_k(input_ids=input_ids, attention_mask=attention_mask)[1]
+                logits = self._dequeue_and_enqueue(k)
 
-        self._dequeue_and_enqueue(k)
+                if len(logits) == 1:
+                    return logits[0]
 
-        # positive logits
-        l_pos = torch.einsum('nc,nc->n', [q, k]).unsqueeze(-1)
+                # Multiply the tensors using torch.einsum
+                if len(logits) >= 2:
+                    negative_keys = torch.einsum('ij, ij -> ij', logits[0], logits[1])
 
-        # negative logits
-        l_neg = torch.einsum('nc,ck->nk', [q, self.queue.clone().detach()])
+                if len(logits) >= 3:
+                    # Loop through the rest of the tensors in the list and multiply with the negative_keys
+                    for i in range(2, len(logits)):
+                        negative_keys = torch.einsum('ij, ik -> ij', negative_keys, logits[i])
 
-        # logits
-        logits = torch.cat([l_pos, l_neg], dim=1)
+                return negative_keys
 
-        return logits
-
+        else:
+            query = self.model_q(input_ids=input_ids, attention_mask=attention_mask)[1]
+            return query
