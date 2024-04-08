a99b78085fceb47a35b338b5960c4a89fd452e13
lremus <leonard.remus@uni-ulm.de>
2024-04-08 13:31:30 +0200

    add queue length to parser arguments

--- run.py
+++ run.py
@@ -664,6 +664,7 @@ def main():
     parser.add_argument("--num_of_negative_samples", default=15, type=int, required=False, help="Number of negative "
                                                                                                 "samples")
     parser.add_argument("--num_of_distractors", default=99, type=int, required=False, help="Number of distractors")
+    parser.add_argument("--queue_length", default=4096, type=int, required=False, help="MoCo queue length")
     parser.add_argument("--GPU", required=False, help="specify the GPU which should be used")
     parser.add_argument("--do_generalisation", default=True, type=bool, required=False)
 
