a0e929d1d1bf56dd7ef5fba15cf0adb50af03937
Leo <leonard.remus@uni-ulm.de>
2024-04-20 00:48:54 +0200

    update runscript

--- run_scripts/moco_InfoNCE.sh
+++ run_scripts/moco_InfoNCE.sh
@@ -6,19 +6,19 @@
 
 # Run the Python script with specified arguments
 
-GPU=$1
+#GPU=$1
 
 python3 ./run.py \
     --loss_function InfoNCE \
     --architecture MoCo \
     --data_path ./data \
-    --lang ruby \
+    --lang python \
     --batch_size 16 \
     --learning_rate 1e-5 \
     --num_train_epochs 5 \
     --train_size 0.8 \
     --data_path ./data/ \
-    --log_level DEBUG \
+    --log_level INFO \
     --num_of_accumulation_steps 16 \
     --num_of_distractors 999 \
-    --GPU "$GPU"
+#    --GPU "$GPU"
