0fc56000e9640750e1db487e42dd86a58f66c83f
Leo <leonard.remus@uni-ulm.de>
2024-04-24 16:00:01 +0200

    more run scripts

---
+++ run_scripts/bw_run_50.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=leonard.remus@uni-ulm.de
+#SBATCH --output=./logging/testout.log
+#SBATCH --error=./logging/testerr.log
+
+
+# Run the Python script with specified arguments
+
+source venv/bin/activate
+
+python3 ./run.py \
+    --loss_function ContrastiveLoss \
+    --architecture Uni \
+    --data_path ./data \
+    --lang php \
+    --batch_size 16 \
+    --learning_rate 1e-5 \
+    --num_train_epochs 5 \
+    --data_path ./data/ \
+    --log_level INFO \
+    --num_of_accumulation_steps 16 \
+    --num_of_distractors 999
