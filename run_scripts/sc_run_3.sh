8578407301ffaf3bb3de5974d53650e78657bac0
Leo <leonard.remus@uni-ulm.de>
2024-04-29 02:49:07 +0200

    updated runscripts

---
+++ run_scripts/sc_run_3.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Run the Python script with specified arguments
+
+GPU=$1
+
+python3 ./run.py \
+    --loss_function InfoNCE \
+    --architecture Uni \
+    --data_path ./data \
+    --lang javascript \
+    --batch_size 8 \
+    --learning_rate 0.00001 \
+    --num_train_epochs 5 \
+    --train_size 0.8 \
+    --data_path ./data/ \
+    --log_level INFO \
+    --num_of_accumulation_steps 16 \
+    --num_of_distractors 999 \
+    --GPU "$GPU"
