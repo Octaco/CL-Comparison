caf006ffd7495101ab49c530a3de1de5972724f6
Leo <leonard.remus@uni-ulm.de>
2024-05-03 16:33:34 +0200

    improve plot visuals

--- run_scripts/sc_run_1.sh
+++ run_scripts/sc_run_1.sh
@@ -7,13 +7,13 @@ python3 ./run.py \
     --loss_function InfoNCE \
     --architecture Uni \
     --data_path ./data \
-    --lang go \
+    --lang python \
     --batch_size 8 \
-    --learning_rate 0.00001 \
+    --learning_rate 1e-5 \
     --num_train_epochs 5 \
     --train_size 0.8 \
     --data_path ./data/ \
     --log_level INFO \
-    --num_of_accumulation_steps 16 \
+    --num_of_accumulation_steps 8 \
     --num_of_distractors 999 \
     --GPU "$GPU"
